3abddabd150f6831856bcf0b1b593e35d26dac2b
Alexandre <alexandre.carvalho@ax4b.com>
2022-01-10 23:24:27 -0300

    tst

--- src/pages/Home/components/Header.js
+++ src/pages/Home/components/Header.js
@@ -2,6 +2,7 @@ import React from "react";
 
 export default function Header(){
     return(
+        
         <header className="header">
             <div className="toolbar">
                 <div>
